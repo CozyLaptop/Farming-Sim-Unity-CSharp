be77eb3873fb73d9ffac7473bf305b1adbe6dc46
Albert <albert.j.dominguez21@gmail.com>
2022-02-10 08:44:39 -0600

    fixed movement speed to be a bit faster

--- PlayerController.cs
+++ PlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float baseSpeed = 2.0f;
+    private float baseSpeed = 4.0f;
     public Vector2 movementDirection;
     public float movementSpeed;
 

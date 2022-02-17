845a6c32fbadf6603dcbd45bdec28ffb7ca928e7
Albert <albert.j.dominguez21@gmail.com>
2022-02-17 14:27:20 -0600

    set hotbar numbers working correctly

---
+++ Hotbar.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hotbar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        setHotbarNumbers();
+    }
+
+    private void setHotbarNumbers()
+    {
+        int count = 1;
+        foreach (HotbarSlot hotbarSlot in GetComponentsInChildren<HotbarSlot>())
+        {
+            if (count != 10)
+            {
+                hotbarSlot.setSlotNumber(count);
+                count++;
+            }
+            else
+            {
+                hotbarSlot.setSlotNumber(0);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

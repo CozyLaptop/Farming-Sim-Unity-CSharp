82339f4c4119e521e867ee43d3de90e27261bc71
Albert <albert.j.dominguez21@gmail.com>
2022-02-18 09:30:04 -0600

    adding some logic for item pickup

--- PlayerController.cs
+++ PlayerController.cs
@@ -5,17 +5,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Vector2 currentLocation;
     private float baseSpeed = 5.0f;
     private Vector2 movementDirection;
     private float movementSpeed;
+
+    private Vector2 playerPosition;
 
     private Rigidbody2D rb;
     private Animator anim;
     private bool playerMoving;
     private Vector2 lastMove;
+    RaycastHit2D hitInfo;
 
-    // Start is called before the first frame update
-    void Start()
+// Start is called before the first frame update
+void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
@@ -53,5 +57,27 @@ public class PlayerController : MonoBehaviour
         anim.SetBool("PlayerMoving", playerMoving);
         anim.SetFloat("LastMoveX", lastMove.x);
         anim.SetFloat("LastMoveY", lastMove.y);
+       
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hitInfo = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            if (hitInfo.collider != null)
+            {
+                Debug.Log("Hit!");
+                Debug.Log(hitInfo.transform.name);
+                if (Vector2.Distance(transform.position, hitInfo.transform.position) < 1)
+                {
+                    Debug.Log("Youre close enough to pick this up!");
+                }
+            }
+
+        }
+    }
+    public Vector2 getPlayerPosition()
+    {
+        return transform.position;
     }
 }

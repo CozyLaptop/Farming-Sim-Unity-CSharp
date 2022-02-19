4415122dd8336853699fca2163d9394e3a23eded
Albert <albert.j.dominguez21@gmail.com>
2022-02-18 23:23:23 -0600

    added id

--- GroundPickableItem.cs
+++ GroundPickableItem.cs
@@ -5,15 +5,4 @@ using UnityEngine;
 public class GroundPickableItem : MonoBehaviour
 {
     public int id;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }

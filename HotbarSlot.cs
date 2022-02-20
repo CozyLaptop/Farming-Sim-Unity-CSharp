e87b3317dc22aabc8b77a0663f26a1748daa91d9
Albert <albert.j.dominguez21@gmail.com>
2022-02-20 12:31:25 -0600

    quantity on hotbar will now set properly

--- HotbarSlot.cs
+++ HotbarSlot.cs
@@ -8,9 +8,11 @@ public class HotbarSlot : MonoBehaviour
     private Sprite slotSprite;
     private Text quantity;
     private Text slotNumber;
+    private Image imageComponent;
 
     private void Awake()
     {
+        imageComponent = transform.GetChild(0).gameObject.GetComponent<Image>();
         slotSprite = transform.GetChild(0).gameObject.GetComponent<Image>().sprite;
         quantity = transform.GetChild(1).gameObject.GetComponent<Text>();
         slotNumber = transform.GetChild(2).gameObject.GetComponent<Text>();
@@ -22,7 +24,8 @@ public class HotbarSlot : MonoBehaviour
     }
     public void setSprite(Sprite sprite)
     {
-        this.slotSprite = sprite;
+        imageComponent.enabled = true;
+        imageComponent.sprite = sprite;
     }
     public Text getSlotNumber()
     {
@@ -33,7 +36,11 @@ public class HotbarSlot : MonoBehaviour
         // This is set from HotBar Script
         this.slotNumber.text = slotNumber.ToString();
     }
-    private void setQuantity(int quantity)
+    public string getQuantity()
+    {
+        return this.quantity.text;
+    }
+    public void setQuantity(int quantity)
     {
         this.quantity.text = quantity.ToString();
     }

2a3cab5114f90c4348a39b22d8aa08bc61f75dcf
Albert <albert.j.dominguez21@gmail.com>
2022-04-05 11:27:50 -0500

    added a way to hand equip the hoe item

--- Player.cs
+++ Player.cs
@@ -7,11 +7,15 @@ public class Player : MonoBehaviour
 {
     private Inventory inventory;
     private Hotbar hotbar;
+    private Item equippedItem;
 
     private float baseSpeed = 5.0f;
     private Vector2 movementDirection;
     private float movementSpeed;
+    private SpriteRenderer spriteRenderer;
     private Vector2 playerPosition;
+    private string lastSpriteName;
+    private Sprite currentSprite;
 
     private Rigidbody2D rb;
     private Animator anim;
@@ -25,8 +29,10 @@ public class Player : MonoBehaviour
     }
     void Start()
     {
+        spriteRenderer = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        equippedItem = null;
     }
     void FixedUpdate()
     {
@@ -58,58 +64,195 @@ public class Player : MonoBehaviour
         anim.SetBool("PlayerMoving", playerMoving);
         anim.SetFloat("LastMoveX", lastMove.x);
         anim.SetFloat("LastMoveY", lastMove.y);
-       
     }
     private void Update()
     {
+        string currentSpriteName = spriteRenderer.sprite.name;
+        if (lastSpriteName != currentSpriteName)
+        {
+            Transform equippedItemTransform = transform.GetChild(1);
+            SpriteRenderer equippedItemGameobjectSpriteRenderer = transform.GetChild(1).gameObject.GetComponent<SpriteRenderer>();
+
+            switch (currentSpriteName)
+            {
+                case "farmerright_0":
+                case "farmerright_4":
+                    equippedItemTransform.localPosition = new Vector3(-0.38f, 0.76f, 0.0f);
+                    equippedItemTransform.eulerAngles = new Vector3(0f, 0f, -80f);
+                    equippedItemGameobjectSpriteRenderer.sortingOrder = 1;
+                    break;
+                case "farmerright_1":
+                case "farmerright_3":
+                    equippedItemTransform.localPosition = new Vector3(-0.26f, 0.76f, 0.0f);
+                    equippedItemTransform.eulerAngles = new Vector3(0f, 0f, -80f);
+                    equippedItemGameobjectSpriteRenderer.sortingOrder = 1;
+                    break;
+                case "farmerright_2":
+                    equippedItemTransform.localPosition = new Vector3(-0.1f, 0.88f, 0.0f);
+                    equippedItemTransform.eulerAngles = new Vector3(0f, 0f, -80f);
+                    equippedItemGameobjectSpriteRenderer.sortingOrder = 1;
+                    break;
+                case "farmerright_5":
+                case "farmerright_7":
+                    equippedItemTransform.localPosition = new Vector3(-0.44f, 0.76f, 0.0f);
+                    equippedItemTransform.eulerAngles = new Vector3(0f, 0f, -80f);
+                    equippedItemGameobjectSpriteRenderer.sortingOrder = 1;
+                    break;
+                case "farmerright_6":
+                    equippedItemTransform.localPosition = new Vector3(-0.44f, 0.82f, 0.0f);
+                    equippedItemTransform.eulerAngles = new Vector3(0f, 0f, -80f);
+                    equippedItemGameobjectSpriteRenderer.sortingOrder = 1;
+                    break;
+                    //
+                case "farmerup_0":
+                case "farmerup_4":
+                    equippedItemTransform.localPosition = new Vector3(0.43f, 0.57f, 0.0f);
+                    equippedItemTransform.eulerAngles = new Vector3(0f, 121f, 2f);
+                    equippedItemGameobjectSpriteRenderer.sortingOrder = 0;
+                    break;
+                case "farmerup_1":
+                case "farmerup_3":
+                    equippedItemTransform.localPosition = new Vector3(0.4f, 0.66f, 0.0f);
+                    equippedItemTransform.eulerAngles = new Vector3(0f, 121f, 2f);
+                    equippedItemGameobjectSpriteRenderer.sortingOrder = 0;
+                    break;
+                case "farmerup_2":
+                    equippedItemTransform.localPosition = new Vector3(0.34f, 0.7f, 0.0f);
+                    equippedItemTransform.eulerAngles = new Vector3(0f, 121f, 2f);
+                    equippedItemGameobjectSpriteRenderer.sortingOrder = 0;
+                    break;
+                case "farmerup_5":
+                case "farmerup_7":
+                    equippedItemTransform.localPosition = new Vector3(0.43f, 0.61f, 0.0f);
+                    equippedItemTransform.eulerAngles = new Vector3(0f, 121f, 2f);
+                    equippedItemGameobjectSpriteRenderer.sortingOrder = 0;
+                    break;
+                case "farmerup_6":
+                    equippedItemTransform.localPosition = new Vector3(0.43f, 0.67f, 0.0f);
+                    equippedItemTransform.eulerAngles = new Vector3(0f, 121f, 2f);
+                    equippedItemGameobjectSpriteRenderer.sortingOrder = 0;
+                    break;
+                //
+                case "farmerleft_0":
+                case "farmerleft_4":
+                    equippedItemTransform.localPosition = new Vector3(-0.17f, 0.82f, 0.0f);
+                    equippedItemTransform.eulerAngles = new Vector3(0f, 180f, -80f);
+                    equippedItemGameobjectSpriteRenderer.sortingOrder = 1;
+                    break;
+                case "farmerleft_1":
+                case "farmerleft_3":
+                    equippedItemTransform.localPosition = new Vector3(-0.05f, 0.82f, 0.0f);
+                    equippedItemTransform.eulerAngles = new Vector3(0f, 180f, -80f);
+                    equippedItemGameobjectSpriteRenderer.sortingOrder = 0;
+                    break;
+                case "farmerleft_2":
+                    equippedItemTransform.localPosition = new Vector3(0.02f, 0.88f, 0.0f);
+                    equippedItemTransform.eulerAngles = new Vector3(0f, 180f, -80f);
+                    equippedItemGameobjectSpriteRenderer.sortingOrder = 0;
+                    break;
+                case "farmerleft_5":
+                case "farmerleft_7":
+                    equippedItemTransform.localPosition = new Vector3(-0.23f, 0.76f, 0.0f);
+                    equippedItemTransform.eulerAngles = new Vector3(0f, 180f, -80f);
+                    equippedItemGameobjectSpriteRenderer.sortingOrder = 1;
+                    break;
+                case "farmerleft_6":
+                    equippedItemTransform.localPosition = new Vector3(-0.27f, 0.82f, 0.0f);
+                    equippedItemTransform.eulerAngles = new Vector3(0f, 180f, -80f);
+                    equippedItemGameobjectSpriteRenderer.sortingOrder = 1;
+                    break;
+                //
+                case "farmerdown_0":
+                case "farmerdown_4":
+                    equippedItemTransform.localPosition = new Vector3(-0.4f, 0.75f, 0.0f);
+                    equippedItemTransform.eulerAngles = new Vector3(0f, 45f, -80f);
+                    equippedItemGameobjectSpriteRenderer.sortingOrder = 1;
+                    break;
+                case "farmerdown_1":
+                case "farmerdown_3":
+                    equippedItemTransform.localPosition = new Vector3(-0.4f, 0.82f, 0.0f);
+                    equippedItemTransform.eulerAngles = new Vector3(0f, 45f, -80f);
+                    equippedItemGameobjectSpriteRenderer.sortingOrder = 1;
+                    break;
+                case "farmerdown_2":
+                    equippedItemTransform.localPosition = new Vector3(-0.4f, 1f, 0.0f);
+                    equippedItemTransform.eulerAngles = new Vector3(0f, 45f, -80f);
+                    equippedItemGameobjectSpriteRenderer.sortingOrder = 1;
+                    break;
+                case "farmerdown_5":
+                case "farmerdown_7":
+                    equippedItemTransform.localPosition = new Vector3(-0.37f, 0.73f, 0.0f);
+                    equippedItemTransform.eulerAngles = new Vector3(0f, 45f, -80f);
+                    equippedItemGameobjectSpriteRenderer.sortingOrder = 1;
+                    break;
+                case "farmerdown_6":
+                    equippedItemTransform.localPosition = new Vector3(-0.3f, 0.77f, 0.0f);
+                    equippedItemTransform.eulerAngles = new Vector3(0f, 45f, -80f);
+                    equippedItemGameobjectSpriteRenderer.sortingOrder = 1;
+                    break;
+                    //
+            }
+        }
+        lastSpriteName = currentSpriteName;
+
+        if (equippedItem != null)
+        {
+            if(equippedItem.getItemName() == "oldHoe")
+            {
+
+            }
+        }
+
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            hotbar.toggleActiveSlot(1);
+            processNumPress(1);
         }
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            hotbar.toggleActiveSlot(2);
+            processNumPress(2);
         }
         if (Input.GetKeyDown(KeyCode.Alpha3))
         {
-            hotbar.toggleActiveSlot(3);
+            processNumPress(3);
         }
         if (Input.GetKeyDown(KeyCode.Alpha4))
         {
-            hotbar.toggleActiveSlot(4);
+            processNumPress(4);
         }
         if (Input.GetKeyDown(KeyCode.Alpha5))
         {
-            hotbar.toggleActiveSlot(5);
+            processNumPress(5);
         }
         if (Input.GetKeyDown(KeyCode.Alpha6))
         {
-            hotbar.toggleActiveSlot(6);
+            processNumPress(6);
         }
         if (Input.GetKeyDown(KeyCode.Alpha7))
         {
-            hotbar.toggleActiveSlot(7);
+            processNumPress(7);
         }
         if (Input.GetKeyDown(KeyCode.Alpha8))
         {
-            hotbar.toggleActiveSlot(8);
+            processNumPress(8);
         }
         if (Input.GetKeyDown(KeyCode.Alpha9))
         {
-            hotbar.toggleActiveSlot(9);
+            processNumPress(9);
         }
         if (Input.GetKeyDown(KeyCode.Alpha0))
         {
-            hotbar.toggleActiveSlot(0);
+            processNumPress(0);
         }
 
         if (Input.GetMouseButtonDown(0))
         {
+
             RaycastHit2D hitInfo = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
             if (hitInfo.collider != null)
             {
                 if (Vector2.Distance(transform.position, hitInfo.transform.position) < 3)
                 {
+                    Debug.Log(hitInfo.transform.name);
                     if (hitInfo.transform.GetComponent<GroundPickableItem>())
                     {
                         int id = hitInfo.transform.GetComponent<GroundPickableItem>().id;
@@ -142,10 +285,6 @@ public class Player : MonoBehaviour
             }
         }
     }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-
-    }
     private void OnTriggerEnter2D(Collider2D trigger)
     {
         //If triggered by a drop item, pick it up
@@ -155,12 +294,6 @@ public class Player : MonoBehaviour
             pickupItem(droppedItem.id);
             Destroy(trigger.gameObject);
         }
-        //If triggered by farmhouse door, ask to sleep
-        //if (trigger.gameObject.name == "door")
-        //{
-        //    //GameObject gameObject = GameObject.Find("TurnInMenu");
-        //    FindObjectOfType<TurnInMenu>().gameObject.SetActive(true);
-        //}
     }
     private void pickupItem(int id)
     {
@@ -169,6 +302,15 @@ public class Player : MonoBehaviour
         {
             hotbar.updateHotbar();
         }
+    }
+    public void processNumPress(int num)
+    {
+        hotbar.toggleActiveSlot(num);
+        equippedItem = inventory.getItemFromIndex(num - 1);
+        if(equippedItem.getItemName() == "oldHoe")
+        {
+            transform.GetChild(1).gameObject.SetActive(true);
+        } else transform.GetChild(1).gameObject.SetActive(false);
     }
     public Vector2 getPlayerPosition()
     {
@@ -190,5 +332,9 @@ public class Player : MonoBehaviour
     {
         this.inventory.addGold(gold);
         UIManager.Instance.updateGold(this.inventory.getGoldAmount());
+    }
+    public void setEquippedItem(Item item)
+    {
+        this.equippedItem = item;
     }
 }
